330392c8af4662d7747b7dfc9a283704fc115280
Johannes Brachem <jbrachem@posteo.de>
2022-09-29 07:50:02 +0000

    version 0.1.0

---
+++ R/main.R
@@ -0,0 +1,241 @@
+
+construct_count_prefix <- function(dir, suffix) {
+  if (stringr::str_detect(suffix, "R$")) {
+    file_has_extension <- stringr::str_detect(fs::dir_ls(dir), "\\.[Rr]$")
+  } else if (stringr::str_detect(suffix, "[Rr][Mm][Dd]$")) {
+    file_has_extension <- stringr::str_detect(fs::dir_ls(dir), "\\.[Rr][Mm][Dd]$")
+  } else if (stringr::str_detect(suffix, "[Qq][Mm][Dd]$")) {
+    file_has_extension <- stringr::str_detect(fs::dir_ls(dir), "\\.[Qq][Mm][Dd]$")
+  } else if (stringr::str_detect(suffix, "py")) {
+    file_has_extension <- stringr::str_detect(fs::dir_ls(dir), "\\.py$")
+  } else {
+    stop(stringr::str_glue("Unknown extension: {suffix}"))
+  }
+
+  pad <- getOption("organizr.padding", default = 3)
+  nfiles <- sum(file_has_extension)
+  prefix <- sprintf(stringr::str_glue("%0{pad}d"), nfiles + 1)
+  prefix
+}
+
+construct_date_prefix <- function(fmt) {
+  now <- Sys.time()
+  prefix <- strftime(now, fmt)
+
+  prefix
+}
+
+construct_filename <- function(prefix, name, suffix) {
+  delim <- getOption("organizr.prefix_delim", default = "_")
+  name <- paste(prefix, name, sep = delim)
+
+  suffix_starts_with_dot <- stringr::str_detect(suffix, "^\\.")
+  if (suffix_starts_with_dot) stop(paste("Invalid suffix:", suffix))
+
+  pattern <- stringr::str_glue("\\.{suffix}$")
+  has_suffix <- stringr::str_detect(name, pattern)
+  if (!has_suffix) {
+    name <- stringr::str_glue("{name}.{suffix}")
+  }
+
+  name
+}
+
+construct_prefix <- function(dir, prefix_by, suffix) {
+  if (is.null(prefix_by)) {
+    prefix_by <- getOption("organizr.prefix_by", default = prefix_by)
+  }
+
+  if (prefix_by == "count") {
+    name_prefix <- construct_count_prefix(dir, suffix)
+  } else if (prefix_by == "date") {
+    fmt <- getOption("organizr.prefix_date_format", default = "%Y-%m-%d")
+    name_prefix <- construct_date_prefix(fmt)
+  } else {
+    msg <- stringr::str_glue("Unrecognized value '{prefix_by}' for argument prefix_by.")
+    stop(msg)
+  }
+
+  name_prefix
+}
+
+check_filepath <- function(path) {
+  if (file.exists(path)) {
+    msg <- stringr::str_glue("The file '{path}' already exists.")
+    stop(msg)
+  }
+}
+
+create_file <- function(
+    name,
+    prefix_by,
+    suffix,
+    directory = NULL,
+    content = "",
+    start_line = 1,
+    proj_path = NULL,
+    open = NULL
+  ) {
+  if (missing(proj_path)) proj_path <- here::here()
+  if (missing(open)) open <- rlang::is_interactive()
+  if (missing(directory)) directory <- suffix
+
+  dir <- fs::path(proj_path, directory)
+  fs::dir_create(dir)
+
+  prefix <- construct_prefix(dir, prefix_by = prefix_by, suffix = suffix)
+  filename <- construct_filename(prefix, name, suffix = suffix)
+  filepath <- fs::path(dir, filename)
+
+  check_filepath(filepath)
+
+  filepath <- fs::path_expand(filepath)
+  fs::file_create(filepath)
+  if (any(content != "")) {
+    readr::write_lines(content, filepath)
+  }
+
+  if (!open) {
+    return(invisible(filepath))
+  }
+
+  if (rstudioapi::hasFun("navigateToFile")) {
+    rstudioapi::navigateToFile(filepath, line = start_line)
+  }
+
+  invisible(filepath)
+}
+
+init_r <- function() {
+  if (!getOption("organizr.r.init_with_date", default = TRUE)) {
+    return()
+  }
+  fmt <- getOption("organizr.r.date_format", default = "%Y-%m-%d %H:%M")
+  now <- Sys.time()
+  timestamp <- strftime(now, fmt)
+  content <- paste0("# Created: ", timestamp, " by {organizr}", "\n")
+  list(content = content, start_line = 3)
+}
+
+init_py <- function() {
+  if (!getOption("organizr.py.init_with_date", default = TRUE)) {
+    return()
+  }
+  fmt <- getOption("organizr.py.date_format", default = "%Y-%m-%d %H:%M")
+  now <- Sys.time()
+  timestamp <- strftime(now, fmt)
+  content <- paste0("# Created: ", timestamp, " by {organizr}", "\n")
+  list(content = content, start_line = 3)
+}
+
+init_qmd <- function(name) {
+  title <- paste("title:", name)
+  output <- paste("format:", "html")
+
+  content <- c("---", title, output, "---")
+
+  list(content = content, start_line = 5)
+}
+
+init_rmd <- function(name) {
+  title <- paste("title:", name)
+  output <- paste("output:", "html_document")
+
+  content <- c("---", title, output, "---")
+
+  list(content = content, start_line = 5)
+}
+
+#' Quickly create and open files with consistent prefixes.
+#'
+#' Orgnizr allows you to set some global options. See
+#' \url{https://github.com/jobrachem/organizr} for details.
+#'
+#' @param name A character string; name of the script.
+#' @param prefix_by Which prefix to use. Can be `"count"` or `"date"`.
+#' @param proj_path Project path.
+#' @param open If `TRUE`, tries to open the file with RStudio after creation.
+#'
+#' @description
+#' `r()` creates an R script.
+#'
+#' `py()` creates a Python script.
+#'
+#' `rmd()` creates an Rmarkdown document.
+#'
+#' `qmd()` creates a Quarto document.
+#'
+#' @returns The filepath as a character string.
+#'
+#' @export
+r <- function(name, prefix_by = c("count", "date"), proj_path = here::here(), open = rlang::is_interactive()) {
+  prefix_by <- match.arg(prefix_by)
+  content <- init_r()
+  filepath <- create_file(
+    name,
+    prefix_by,
+    suffix = "R",
+    directory = getOption("organizr.r.directory", default = "R"),
+    content = content$content,
+    start_line = content$start_line,
+    proj_path = proj_path,
+    open = open
+  )
+
+  invisible(filepath)
+}
+
+#' @export
+#' @rdname r
+qmd <- function(name, prefix_by = c("count", "date"), proj_path = here::here(), open = rlang::is_interactive()) {
+  prefix_by <- match.arg(prefix_by)
+  content <- init_qmd(name)
+  filepath <- create_file(
+    name,
+    prefix_by,
+    suffix = "qmd",
+    directory = getOption("organizr.qmd.directory", default = "qmd"),
+    content = content$content,
+    start_line = content$start_line,
+    proj_path = proj_path,
+    open = open
+  )
+  invisible(filepath)
+}
+
+#' @export
+#' @rdname r
+rmd <- function(name, prefix_by = c("count", "date"), proj_path = here::here(), open = rlang::is_interactive()) {
+  prefix_by <- match.arg(prefix_by)
+  content <- init_rmd(name)
+  filepath <- create_file(
+    name,
+    prefix_by,
+    suffix = "rmd",
+    directory = getOption("organizr.rmd.directory", default = "rmd"),
+    content = content$content,
+    start_line = content$start_line,
+    proj_path = proj_path,
+    open = open
+  )
+  invisible(filepath)
+}
+
+
+#' @export
+#' @rdname r
+py <- function(name, prefix_by = c("count", "date"), proj_path = here::here(), open = rlang::is_interactive()) {
+  prefix_by <- match.arg(prefix_by)
+  content <- init_py()
+  filepath <- create_file(
+    name,
+    prefix_by,
+    suffix = "py",
+    directory = getOption("organizr.py.directory", default = "py"),
+    content = content$content,
+    start_line = content$start_line,
+    proj_path = proj_path,
+    open = open
+  )
+  invisible(filepath)
+}
---
+++ README.md
@@ -0,0 +1,129 @@
+
+# Organizr: Tidy up your projects for good!
+
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/jobrachem/organizr/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/jobrachem/organizr/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
+Organizr provides opinionated code-shortcuts to quickly create R scripts, as well as 
+minimal Quarto and Rmarkdown documents with a consistent naming scheme.
+
+Organizr offers a count-based prefix...
+
+```
+R/
+  001_first-script.R
+  002_another-script.R
+  003_and-so-on.R
+```
+
+... and a date-based prefix.
+
+```
+R/
+  2022-09-24_first-script.R
+  2022-09-26_another-script.R
+  2022-09-26_and-so-on.R
+```
+
+## Installation
+
+You can install the development version of organizr like so:
+
+``` r
+# install.packages("devtools") # if you do not have devtools installed
+devtools::install_github("jobrachem/organizr")
+```
+
+## Some details
+
+Organizr offers the four following functions:
+
+- `r()` creates an R script with a timestamp in `project_path/R`.
+- `py()` creates a Python script with a timestamp in `project_path/py`.
+- `qmd()` creates a minimal Quarto document in `project_path/qmd`.
+- `rmd()` creates a minimal Quarto document in `project_path/rmd`.
+
+Organizr is an opinionated packaged. It may very well be that it does not fit
+your personal workflow, and that's ok. 
+
+## Setup
+
+The best user experience arises, if you load `organizr` in the `.Rprofile` of
+your current project. To do so, you can use the following line to open your
+project's .Rprofile:
+
+```r
+# install.packages("usethis")
+usethis::edit_r_profile(scope = "project")
+```
+
+Then place the following code inside the .Rprofile:
+
+``` r
+if (interactive()) {
+  suppressMessages(require(organizr))
+}
+```
+
+This will always load up the library `organizr` in interactive R sessions. 
+Now, you will always be able to create consistently named new R scripts via
+quick and simple function calls like
+
+```r
+r("my-script")
+```
+
+If this is the first R file in your project, this function call will create
+the file `001_my-script.R` in the directory `project_path/R`.
+
+
+## Options
+
+You can set some global options via 
+
+```r
+options("option_name" = "option_value")
+```
+
+If you want to use options, it often makes sense to also place them directly in
+the .Rprofile file.
+
+Example:
+
+With this function call, you set the default prefix used by `organizr` to "date":
+
+```r
+options("organizr.prefix_by" = "date")
+```
+
+### General options:
+
+| Option | Meaning |
+| --- | --- |
+| `organizr.prefix_delim` | Which character to insert as a separator between the prefix and the actual file name. The default is `"_"`|
+| `organizr.prefix_by` | Can be used to override the function default of `"count"`. Can be `"count"` or `"date"`. |
+| `organizr.prefix_date_format` | Date format for date prefixes. Can be any format string accepted by `strftime`. Default is `"%Y-%m-%d"`. |
+
+### Options for R scripts:
+
+| Option | Meaning |
+| --- | --- |
+| `organizr.r.init_with_date` | Can be set to `FALSE` to turn off the inclusion of the timestamp comment at the top of the script. | 
+`organizr.r.date_format` | Date format for R and Python script timestamp comment. Can be any format string accepted by `strftime`. Default is `"%Y-%m-%d %H:%M"`. |
+`organizr.r.directory` | The directory in which the scripts should be place (relative to the project directory) |
+
+### Options for Python scripts:
+
+| Option | Meaning |
+| --- | --- |
+| `organizr.py.init_with_date` | Can be set to `FALSE` to turn off the inclusion of the timestamp comment at the top of the script. | 
+`organizr.py.date_format` | Date format for R and Python script timestamp comment. Can be any format string accepted by `strftime`. Default is `"%Y-%m-%d %H:%M"`. |
+`organizr.py.directory` | The directory in which the scripts should be place (relative to the project directory) |
+
+### Other options:
+
+| Option | Meaning |
+| --- | --- |
+| `organizr.rmd.directory` | The directory in which `.Rmd` files should be place (relative to the project directory) |
+| `organizr.qmd.directory` | The directory in which `.qmd` files should be place (relative to the project directory) |
